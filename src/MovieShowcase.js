e922b2350bb2efad883b73c70ba2bbc8ce85e37e
Nate Miller <natekmiller96@gmail.com>
2018-10-30 17:12:23 +0000

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -5,7 +5,10 @@ import movieData from './data.js'
 export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
-    // map over your movieData array and return the correct 
+    const movieCards = movieData.map((movie) =>
+    <li key={movie.id}><MovieCard title={movie.title} IMDBRating={movie.IMDBRating} genres={movie.genres} poster={movie.poster} /></li>
+  )
+   return <ul>{movieCards}</ul>
   }
 
   render() {
--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -13,7 +13,7 @@ import React, { Component } from 'react';
 import CardFront from './CardFront.js';
 import CardBack from './CardBack.js';
 
-
+// eslint-disable-next-line
 const posterMap = {
   'choux-maru-istanbul': cmi,
   'choux-maru-part-1': cmp1,
@@ -32,12 +32,16 @@ export default class MovieCard extends Component {
   render() {
     return (
       <div className="movie-card">
-        {/* which component should receive which props? */}
-        <CardFront />
-        <CardBack />
+      <CardFront poster={this.poster}/>
+      <CardBack title={this.title} genres={this.genres} IMDBRating={this.IMDBRating}/>
       </div>
     )
   }
 }
 
-// Don't forget your default props!
+MovieCard.defaultProps = {
+  title: 'Unknown',
+  IMDBRating: null,
+  genres: ["No Genre(s) Found"],
+  poster: 'default'
+}
